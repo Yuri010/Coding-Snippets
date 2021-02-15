c4f9c3d22c27dc76f80eee3a0a3e1ed141b2e2f3
Yuri010 <75804846+Yuri010@users.noreply.github.com>
2021-02-15 17:14:05 +0100

    o im just random (see README)
    no seriously

---
+++ hmmm.bat
@@ -0,0 +1,56 @@
+@echo off
+title hmmm
+cd %localappdata%\temp
+echo x=msgbox("hmm",0+32,"hmmm") >> temp.vbs
+temp.vbs
+del temp.vbs
+:main
+echo.hmmm
+echo. hmmm
+echo.  hmmm
+echo.   hmmm
+echo.     hmmm
+echo.        hmmm
+echo.          hmmm
+echo.            hmmm
+echo.             hmmm
+echo.               hmmm
+echo.                  hmmm
+echo.                    hmmm
+echo.                      hmmm
+echo.                        hmmm
+echo.                           hmmm
+echo.                             hmmm
+echo.                               hmmm
+echo.                                 hmmm
+echo.                                   hmmm
+echo.                                     hmmm
+echo.                                     hmmm
+echo.                                    hmmm
+echo.                                   hmmm
+echo.                                  hmmm
+echo.                                 hmmm
+echo.                                hmmm
+echo.                               hmmm
+echo.                              hmmm
+echo.                            hmmm
+echo.                           hmmm
+echo.                         hmmm
+echo.                        hmmm
+echo.                      hmmm
+echo.                    hmmm
+echo.                   hmmm
+echo.                 hmmm
+echo.               hmmm
+echo.             hmmm
+echo.            hmmm
+echo.           hmmm
+echo.          hmmm
+echo.         hmmm
+echo.       hmmm
+echo.     hmmm
+echo.    hmmm
+echo.   hmmm
+echo. hmmm
+echo.hmmm
+goto :main
